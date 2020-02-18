d6bbec6b7a09abe839d28a50c5e40ceb5afa2974
elbum <elbum@hanmail.net>
2020-02-18 22:33:14 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import TOC from './components/TOC';
 import Subject from './components/Subject'
 import Content from './components/Content'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      subject:{title:'WEB',sub:'World wide Web state'},
-      contents:[
-        {id:1,title:'HTML',desc:'HTML is hypertect'},
-        {id:2,title:'CSS',desc:'CSS is for design'},
-        {id:3,title:'Javascript',desc:'Javascript is for interactive'},
+      mode: "welcome",
+      welcome: { title: "Welcome", desc: "Hello, React!~!" },
+      subject: { title: 'WEB', sub: 'World wide Web state' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is hypertect' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'Javascript', desc: 'Javascript is for interactive' },
       ],
     }
 
@@ -20,7 +22,17 @@ class App extends Component {
 
 
   render() {
-    
+    var _title, _desc;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+
+    }
+
     return (<div className="App">
       <Subject tittle={this.state.subject.title} sub={this.state.subject.sub}>
         hello sub
@@ -31,7 +43,7 @@ class App extends Component {
       <br></br><br></br>
       Hello React
       <TOC data={this.state.contents}></TOC>
-      <Content title="HTML Props." sub="Props. HTML is hypertext markup Language."></Content>
+      <Content title={_title} sub={_desc}></Content>
     </div>)
   }
 }
