eecc6b530d37cf9bcf58e774680b7d455dfa0934
elbum <elbum@hanmail.net>
2020-02-20 22:17:26 +0900

    create contents2

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
         console.log(_title, _desc);
         this.max_content_id = this.max_content_id + 1;
 
-        // for performance use concat
+        // for performance use concat 
         var _contents = this.state.contents.concat({ id: this.max_content_id, title: _title, desc: _desc });
         this.setState(
           { contents: _contents }
